d2c3431ec7980d82c9b174459a5aab2c08f24259
Patrick Watanabe <77561694+kochingko@users.noreply.github.com>
2021-01-17 12:44:25 +0800

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This assignment is all about getting the inverse of a matrix by catching
+## the result within a lexical scope of a function.
 
-## Write a short comment describing this function
+## The function below creates a unique and new environment.
 
 makeCacheMatrix <- function(x = matrix()) {
+	inv <- NULL
+	set <- function (y){
+	 x <<- y
+	 inv <<- NULL
+}
+	get <- function () x
+
+	setinverse <- function(inverse) inv <<- inverse	
+	getinverse <- function()inv
+	list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 
 }
 
-
-## Write a short comment describing this function
+## The function 'cacheSolve' allows returning the inverse of matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	inv <- x$getinverse()
+	if(!is.null(inv)) {
+	 message("getting cached data")
+	 return(inv)
+	}
+	data <- x$get()
+	inv <- solve(data, ...)
+	x$setinverse(inv)
+	inv
+}
 }
